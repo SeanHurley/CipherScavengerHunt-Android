026fd716e93b33a91d4a739e964c306c456eeaf5
Sean Hurley <sean.hurley6@gmail.com>
2012-08-20 12:27:35 -0500

    Added git ignore

--- bin/HomeActivity-debug-unaligned.apk.d
+++
@@ -1,6 +0,0 @@
-/home/sean/workspace/cipher-scavenger-hunt-android/bin/HomeActivity-debug-unaligned.apk : \
-/home/sean/workspace/cipher-scavenger-hunt-android/bin/HomeActivity.ap_ \
-/home/sean/workspace/cipher-scavenger-hunt-android/bin/classes.dex \
-/home/sean/android-sdks/tools/support/annotations.jar \
-/home/sean/workspace/cipher-scavenger-hunt-android/libs/json-smart-1.1.1.jar \
-/home/sean/workspace/cipher-scavenger-hunt-android/libs/android-support-v4.jar \
